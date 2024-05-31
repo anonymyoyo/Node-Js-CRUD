d2e6b44c02b867d7cf28b0ff2788cbe19f457f17
anonymyoyo <paulemmanuel@gmail.com>
2024-05-30 22:36:02 -0700

    express

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const exphdbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 500;

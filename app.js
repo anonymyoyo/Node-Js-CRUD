59398446bd124c2fb859cd3ef55e43a6725d9c01
anonymyoyo <paulemmanuel@gmail.com>
2024-05-31 14:56:31 -0700

    2e jour

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
 
 // Parsing middleware
 // Parse application/x-www-form-urlencoded

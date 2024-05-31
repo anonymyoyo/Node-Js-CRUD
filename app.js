c1fce2afc3c58c8a6a0753308bb68402bb4081a3
anonymyoyo <paulemmanuel@gmail.com>
2024-05-31 15:30:50 -0700

    Parse application/json

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const exphdbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1000;
 
 // Parsing middleware
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(port, ()=> console.log('Listening on port ${port}'));
+app.use(bodyParser.json());
+
+app.listen(port, ()=> console.log("Listening on port "+port));

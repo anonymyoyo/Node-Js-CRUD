ba2b541932aeaf9d926d7e4ef4a48bcb7375dd3d
anonymyoyo <paulemmanuel@gmail.com>
2024-05-31 15:58:25 -0700

    creation du dossier layouts ainsi que notre propre extension(.hbs)

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(bodyParser.json());
 // Permet d'ajouter une liaison vers un fichier static et ici nous avons choisi le doc public
 app.use(express.static('public'));
 
-
+// Template enginr
+app.engine('hbs', exphbs({extname:'.hbs'}));
+app.set('view engine', 'hbs');
 
 app.listen(port, ()=> console.log("Listening on port "+port));

d9491503e645c983544e20d1af2987ae0178e3b4
anonymyoyo <paulemmanuel@gmail.com>
2024-06-02 11:52:09 -0700

    traitement de la  methode get

--- methods/get-request.js
+++ methods/get-request.js
@@ -1,1 +1,12 @@
-module.exports=(req, res) =>{};
+module.exports=(req, res) =>{
+    if (req.url === "/api/movies") {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(req.movies));
+        res.end();
+    }
+    else{
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({title: 'NOT FOUND', message:'Route Not Found'}));
+    }
+};

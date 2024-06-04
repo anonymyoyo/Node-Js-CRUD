63240df65d454760d4ec7968a9bcd30055f37889
anonymyoyo <paulemmanuel@gmail.com>
2024-06-04 12:09:59 -0700

    traitement de la methode post:body-parser

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,1 +1,9 @@
-module.exports=(req, res) =>{};
+module.exports=(req, res) =>{
+    if (req.url === '/api/movies'){
+        try {
+            console.log('Request Body: ', req.body);
+        } catch (error) {
+            
+        }
+    }
+};

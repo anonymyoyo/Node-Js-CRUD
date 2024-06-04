09b778c4c28209262301d430a1a07a6ce5d6bb87
anonymyoyo <paulemmanuel@gmail.com>
2024-06-04 15:05:35 -0700

    traitement de la methode delete

--- methods/delete-request.js
+++ methods/delete-request.js
@@ -1,1 +1,7 @@
-module.exports=(req, res) =>{};
+module.exports=(req, res) =>{
+    let baseUrl = req.url.substring(0, req.url.lastIndexOf("/") + 1);
+    // console.log(baseUrl);
+    let id = req.url.split("/")[3];
+    const regexV4 = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{3}-[89AB] [0-9A-F]{3}-[0-9A-F]{12}$/i);
+    
+};
